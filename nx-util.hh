57349eaf07510b64b29e69a4cd711da1826010ac
darkelf44 <darkelf44@outlook.com>
2018-05-12 14:57:47 +0200

    Finally renaming data to items in List. Introducing data() methods.

--- nx-util.hh
+++ nx-util.hh
@@ -16,14 +16,14 @@ public:
 
 	// Constructors & destructors
 	List() noexcept
-		: n(0), m(0), data(nullptr) {}
+		: n(0), m(0), items(nullptr) {}
 	List(List<T> && list) noexcept
-		: n(list.n), m(list.m), data(list.data) {list.n = 0; list.m = 0; list.data = nullptr;}
+		: n(list.n), m(list.m), items(list.items) {list.n = 0; list.m = 0; list.items = nullptr;}
 	List(const List<T> & list);
 	~List() override
-		{nx::type::destroyArrayAt(data, n); nx::type::free(data);}
+		{nx::type::destroyArrayAt(items, n); nx::type::free(items);}
 		
-	// Size & capacity	
+	// Size & capacity
 	inline size_t size() const
 		{return n;}
 	inline size_t capacity() const
@@ -33,9 +33,15 @@ public:
 	void reserve(size_t n);
 	void compact();
 	
+	// Data
+	inline T * data()
+		{return items;}
+	inline const T * data() const
+		{return items;}
+	
 	// Getters
 	inline const T & get(size_t i, const T & def = T())
-		{return (0 <= i) && (i < n) ? data[i] : def;}
+		{return (0 <= i) && (i < n) ? items[i] : def;}
 		
 	// Methods
 	void append(T && item);
@@ -46,38 +52,42 @@ public:
 	
 	// Itertator methods
 	inline T * begin() noexcept
-		{return data;}
+		{return items;}
 	inline const T * begin() const noexcept
-		{return data;}
+		{return items;}
 	inline T * end() noexcept
-		{return data + n;}
+		{return items + n;}
 	inline const T * end() const noexcept
-		{return data + n;}
-		
+		{return items + n;}
+	
+	// Copy & move
+	List<T> & operator = (List<T> && list) noexcept;
+	List<T> & operator = (const List<T> & list);
+	
 	// Operators
 	inline T & operator [] (size_t i) noexcept
-		{return data[i];}
+		{return items[i];}
 	inline const T & operator [] (size_t i) const noexcept
-		{return data[i];}
+		{return items[i];}
 		
 	inline List<T> & operator += (List<T> && list)
 		{extend(static_cast<List<T> &&>(list)); return * this;}
 	inline List<T> & operator += (const List<T> & list)
 		{extend(list); return * this;}
-
+		
 private:
 	size_t n;
 	size_t m;
-	T * data;
+	T *    items;
 };
 
 template<typename T> List<T>::List(const List<T> & list)
-	: nx::Object(), n(0), m(0), data(nullptr)
+	: nx::Object(), n(0), m(0), items(nullptr)
 {
 	if (list.n > 0)
 	{
 		reserve(list.n);
-		nx::type::createArrayAtByCopy(data, list.data, list.n);
+		nx::type::createArrayAtByCopy(items, list.items, list.n);
 		n = list.n;
 	}
 }
@@ -87,12 +97,12 @@ template<typename T> void List<T>::resize(size_t size)
 	reserve(size);
 	if (size > n)
 	{
-		nx::type::createArrayAt(data + n, size - n);
+		nx::type::createArrayAt(items + n, size - n);
 		n = size;
 	}
 	if (size < n)
 	{
-		nx::type::destroyArrayAt(data + size, n - size);
+		nx::type::destroyArrayAt(items + size, n - size);
 		n = size;
 	}
 }
@@ -102,37 +112,45 @@ template<typename T> void List<T>::reserve(size_t size)
 	if (size > m)
 	{
 		List list;
-		list.data = nx::type::alloc<T>(sizeof(T) * size);
-		nx::type::confirm(list.data);
+		list.items = nx::type::alloc<T>(sizeof(T) * size);
+		nx::type::confirm(list.items);
 		
-		if (data)
+		if (items)
 		{
-			nx::type::createArrayAtByMove(list.data, data, n);
-			nx::type::destroyArrayAt(data, n);
-			nx::type::free(data);
+			nx::type::createArrayAtByMove(list.items, items, n);
+			nx::type::destroyArrayAt(items, n);
+			nx::type::free(items);
 		}
 		
 		m = size;
-		data = list.data;
-		list.data = nullptr;
+		items = list.items;
+		list.items = nullptr;
 	}
 }
 
 template<typename T> void List<T>::compact()
 {
-	if (data)
+	if (items)
 	{
-		List list;
-		list.data = nx::type::alloc<T>(sizeof(T) * n);
-		nx::type::confirm(list.data);
-		
-		nx::type::createArrayAtByMove(list.data, data, n);
-		nx::type::destroyArrayAt(data, n);
-		nx::type::free(data);
-		
-		m = n;
-		data = list.data;
-		list.data = nullptr;
+		if (n > 0)
+		{
+			List list;
+			list.items = nx::type::alloc<T>(sizeof(T) * n);
+			nx::type::confirm(list.items);
+			
+			nx::type::createArrayAtByMove(list.items, items, n);
+			nx::type::destroyArrayAt(items, n);
+			nx::type::free(items);
+			
+			m = n;
+			items = list.items;
+			list.items = nullptr;
+		}
+		else
+		{
+			m = 0;
+			items = nullptr;
+		}
 	}
 }
 
@@ -145,7 +163,7 @@ template<typename T> void List<T>::append(T && item)
 		reserve(m + (m >> 1));
 	}
 	
-	nx::type::createAt(data + n, static_cast<T &&>(item));
+	nx::type::createAt(items + n, static_cast<T &&>(item));
 	n ++;
 }
 
@@ -158,7 +176,7 @@ template<typename T> void List<T>::append(const T & item)
 		reserve(m + (m >> 1));
 	}
 	
-	nx::type::createAt(data + n, item);
+	nx::type::createAt(items + n, item);
 	n ++;
 }
 
@@ -173,7 +191,7 @@ template<typename T> void List<T>::extend(List<T> && list)
 		}
 		reserve(x);
 
-		nx::type::createArrayAtByMove(data + n, list.data, list.n);
+		nx::type::createArrayAtByMove(items + n, list.items, list.n);
 		n += list.n;
 	}
 }
@@ -189,12 +207,25 @@ template<typename T> void List<T>::extend(const List<T> & list)
 		}
 		reserve(x);
 
-		nx::type::createArrayAtByCopy(data + n, list.data, list.n);
+		nx::type::createArrayAtByCopy(items + n, list.items, list.n);
 		n += list.n;
 	}
 }
 
+template<typename T> List<T> & List<T>::operator = (List<T> && list) noexcept
+{
+	n = list.n; list.n = 0;
+	m = list.m; list.m = 0;
+	items = list.items; list.items = nullptr;
+	return * this;
+}
 
+template<typename T> List<T> & List<T>::operator = (const List<T> & list)
+{
+	resize(0);
+	extend(list);
+	return * this;
+}
 
 // Set class - TODO: concurrent, lock free, virtual interface
 template<typename T> class Set: public Object
